b12b27d01c8b3078f0566d35f4da6db832307ce8
Aysu <272784946+qan-agency@users.noreply.github.com>
2026-04-01 17:14:57 +0200

    add github deploy_ rearrange the pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Menu from "./landing/Menu.svelte";
+
+</script>
+<main class="flex flex-col p-[10%] pb-0 uppercase font-extralight">
+    <h1 class="text-[128px] tracking-[40%] font-chakra leading-[1] font-extralight">QAN</h1>
+    <Menu className="ml-[12px]" />
+    <div class="flex flex-col w-full mt-[96px] gap-[16px]">
+        <h1 class="text-4xl tracking-[50%]">We gather data, you make the conclusions_</h1>
+    </div>
+</main>
