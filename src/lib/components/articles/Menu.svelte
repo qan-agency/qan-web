c72e0f7916a22f99b3746d203536c9b11443c8d4
Aysu <272784946+qan-agency@users.noreply.github.com>
2026-04-04 01:00:50 +0200

    add Menu

---
+++ src/lib/components/articles/Menu.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    const routes = [
+        { title: 'Home', url: '/' },
+        { title: 'Articles', url: '/articles' }
+    ];
+</script>
+
+<div class="flex flex-col gap-12 bg-black w-60 h-screen border-r 
+            border-[#252525] p-12">
+    <h1 class="text-[32px] tracking-[20%]">QAN___</h1>
+    <div class="flex flex-col">
+    {#each routes as route (route.title)}
+        <button onclick={() => window.location.href = route.url } 
+                class="text-left h-12">{ route.title }</button>
+    {/each}
+    </div>
+</div>
